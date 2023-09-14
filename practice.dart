2e8cb42a2d1447ee6bb4051053c4d232f2b3e2fa
john ormartii <johnormartii@gmail.com>
2023-09-14 09:53:06 +0300

    this is my assignment

---
+++ practice.dart
@@ -0,0 +1,26 @@
+//prsctice in data types learned
+
+void main() {
+  int num1 = 6;
+  int num2 = 2;
+  print(num1 + num2);
+  print(num1 * num2);
+  print(num1 / num2);
+  print(num1 - num2);
+
+  var name = ("john ormartii");
+  print(name);
+
+  //list
+  var num_list = (1, 2, 3, 4, 4);
+  print(num_list);
+
+  //bool
+  bool test;
+  test = 6 > 4;
+  print(test);
+
+  //string
+  String str1 = 'this is a sungle line string';
+  print(str1);
+}
